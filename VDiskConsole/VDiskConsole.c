633b4908d7dd263678be17f1dd39e913e417d1fd
ika <287720499@qq.com>
2022-04-02 23:28:10 +0800

    创建了用户交互界面程序的工程

---
+++ VDiskConsole/VDiskConsole.c
@@ -0,0 +1,14 @@
+﻿#include <winsock2.h>
+#include <windows.h>
+#include <winioctl.h>
+#include <shlobj.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "vdisk.h"
+
+#pragma prefast( disable: 28719, "this warning only applies to drivers not applications" )		//忽略警告
+
+int main(int argc, char* argv[]) {
+
+
+}
